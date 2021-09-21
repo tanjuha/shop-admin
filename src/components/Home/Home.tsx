083890eb069e140f67d1202d2d7ce88cd1195274
Tetiana Kovalenko <tkovale@softserveinc.com>
2021-09-21 20:35:02 +0300

    add home img

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Home = () => {
     return (
+        <>
         <h3>Home</h3>
+        <img className="d-block m-auto" src="./images/home.webp" alt="home" />
+        </>
     )
 }
 
