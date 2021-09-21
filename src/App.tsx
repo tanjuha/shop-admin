8e235b9a601e996c0b607bf2959bfa439a8a68b0
Tetiana Kovalenko <tkovale@softserveinc.com>
2021-09-21 14:40:36 +0300

    add css grid app v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import React from "react";
 import Footer from "./components/Footer/Footer";
 import Sidebar from "./components/Sidebar/Sidebar";
-import "./app.css";
+import "./style.css";
 
 function App() {
   return (
-    <div className="app-container">
-      <div className="d-flex align-items-stretch app-content">
-      <Sidebar />
-      <div className="d-flex w-100">
-        content
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad, dolor cum nam quidem corrupti atque error repellat aspernatur possimus harum quo consectetur nemo ut sed assumenda praesentium id et alias qui hic nulla! Numquam doloremque beatae voluptas possimus eligendi molestias eum a enim velit placeat, repudiandae repellat quo tempora vitae maxime iusto rem minima maiores iure corporis fugit totam. Sapiente sequi ipsa placeat temporibus optio esse quam quasi amet delectus dolorem. Nemo labore ab fuga tempore exercitationem pariatur sed ad, doloribus error possimus obcaecati vero porro beatae mollitia quidem deleniti iste a quia ex delectus modi quis! Consectetur, culpa perspiciatis.
+    <div className="wrapper">
+      <div className="d-flex align-items-stretch">
+        <Sidebar />
+        <div className="w-100">
+          <div className="content-container">
+            <div className="content-page">
+            content Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Ad, dolor cum nam quidem corrupti atque error repellat aspernatur
+              possimus harum quo consectetur nemo ut sed assumenda praesentium
+              id et alias qui hic nulla! Numquam doloremque beatae voluptas
+              possimus eligendi molestias eum a enim velit placeat, repudiandae
+              repellat quo tempora vitae maxime iusto rem minima maiores iure
+              corporis fugit totam. Sapiente sequi ipsa placeat temporibus optio
+              esse quam quasi amet delectus dolorem. Nemo labore ab fuga tempore
+              exercitationem pariatur sed ad, doloribus error possimus obcaecati
+            
+            </div>
+            <Footer />
+          </div>
         </div>
       </div>
-      <Footer />
     </div>
   );
 }
