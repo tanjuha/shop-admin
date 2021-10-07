0567a4669b1ac465188859aafa19a2338ebb88bf
Tetiana Kovalenko <tkovale@softserveinc.com>
2021-10-07 12:15:32 +0300

    add createAsyncThunc

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./style.scss";
+import { fetchProducts } from "./../../store/slices/productReducer";
+import { Spinner } from "react-bootstrap";
 
 const Products = () => {
-  return <h4 className="page-title">All products</h4>;
+  const dispatch = useDispatch();
+
+  const { status, error } = useSelector((state: any) => state.products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  const products = useSelector((state: any) => state.products.products);
+
+  return (
+    <>
+      <h4 className="page-title">All products</h4>
+      {status === "loading" && <Spinner animation="border" />}
+      {error && <h4>Something wrong ... </h4>}
+      {products && (
+        <ul>
+          {products.map((product: any) => {
+            return <li key={product.email}>{product.email}</li>;
+          })}
+        </ul>
+      )}
+    </>
+  );
 };
 
 export default Products;
--- src/store/slices/productReducer.ts
+++ src/store/slices/productReducer.ts
@@ -1,4 +1,28 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchProducts: any = createAsyncThunk(
+  "products/fetchProducts",
+  async function (_, thunkAPI) {
+    try {
+      const response = await fetch(`https://randomuser.me/api/?results=10`);
+
+      if (!response.ok) {
+        throw new Error("Error :( ");
+      }
+
+      const { results } = await response.json();
+
+      return results;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+const setError = (state: any, action: any) => {
+  state.status = "reject";
+  state.error = action.payload;
+};
 
 export interface ProductState {
   products: any;
@@ -16,6 +40,17 @@ export const productSlice = createSlice({
       state.products = [...state.products, action.payload];
       localStorage.setItem("products", JSON.stringify(state.products));
     },
+  },
+  extraReducers: {
+    [fetchProducts.pending]: (state: any) => {
+      state.status = "loading";
+      state.error = null;
+    },
+    [fetchProducts.fulfilled]: (state: any, action: any) => {
+      state.status = "resolved";
+      state.products = action.payload;
+    },
+    [fetchProducts.rejected]: setError,
   },
 });
 
